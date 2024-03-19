4207f9a01a04b2fdc12c1a7ebe236ea8b8137ca3
Marina Pintado <darkhuoyt@gmail.com>
2024-03-19 14:58:40 +0100

    Header component life cycle

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
+  constructor() {
+    console.log('cosntructor');
+  }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChanges');
+  }
+
+  ngOnInit(): void {
+    console.log('OnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('DoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('OnDestroy');
+  }
 }
