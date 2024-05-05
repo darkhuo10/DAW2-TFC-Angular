82a87b8cb4adc123f2289ad76228ae077b828adc
Marina Pintado <darkhuoyt@gmail.com>
2024-05-05 19:54:15 +0200

    Rating system

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './rating.component.scss'
 })
 export class RatingComponent {
-
+  starRating: number = 3; // Será sacado de la base de datos
+  stars: number[] = Array.from({ length: 5 }, (_, i) => i + 1);
 }
