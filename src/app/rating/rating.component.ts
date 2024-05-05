d46f6f731e1744d979b4410d909f0918a9d7f53d
Marina Pintado <darkhuoyt@gmail.com>
2024-05-05 21:21:44 +0200

    Working rating

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -7,6 +7,17 @@ import { Component, Input } from '@angular/core';
 })
 export class RatingComponent {
   @Input() rating: number = 0; 
-  
+  @Input() disabled: boolean = false;
+
   stars: number[] = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  updateRating(star: number): void {
+    if (!this.disabled) {
+      if (this.rating === star) {
+        this.rating = 0;
+      } else {
+        this.rating = star;
+      }
+    }
+  }
 }
