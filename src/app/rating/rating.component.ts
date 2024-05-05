4789b078342195e5937644bf66ad07e0f4bdfd1c
Marina Pintado <darkhuoyt@gmail.com>
2024-05-05 21:14:08 +0200

    typo corrected

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -3,10 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
-  styleUrls: ['./rating.component.scss'] // Fixed typo here
+  styleUrl: './rating.component.scss'
 })
 export class RatingComponent {
-  @Input() rating: number = 0; // Initialize with default value
+  @Input() rating: number = 0; 
   
   stars: number[] = Array.from({ length: 5 }, (_, i) => i + 1);
 }
