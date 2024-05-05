4aa61c3aa886573bd2b1616ce9a20fad4e8de95e
Marina Pintado <darkhuoyt@gmail.com>
2024-05-05 21:36:47 +0200

    more games added //fake data

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -41,6 +41,109 @@ export class MainComponent {
       price: 0.99, 
       rating: 3.8 
     },
+    { 
+      name: 'Game 5', 
+      description: 'Description for Game 5', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 19.99, 
+      rating: 4.5 
+    },
+    { 
+      name: 'Game 6', 
+      description: 'Description for Game 6', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 29.99, 
+      rating: 4.8 
+    },
+    { 
+      name: 'Game 7', 
+      description: 'Description for Game 7', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 45.50, 
+      rating: 2.3 
+    },
+    { 
+      name: 'Game 8', 
+      description: 'Description for Game 8', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 0.99, 
+      rating: 3.8 
+    },{ 
+      name: 'Game 9', 
+      description: 'Description for Game 9', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 19.99, 
+      rating: 4.5 
+    },
+    { 
+      name: 'Game 10', 
+      description: 'Description for Game 10', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 29.99, 
+      rating: 4.8 
+    },
+    { 
+      name: 'Game 12', 
+      description: 'Description for Game 12', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 45.50, 
+      rating: 2.3 
+    },
+    { 
+      name: 'Game 13', 
+      description: 'Description for Game 13', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 0.99, 
+      rating: 3.8 
+    },{ 
+      name: 'Game 14', 
+      description: 'Description for Game 14', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 19.99, 
+      rating: 4.5 
+    },
+    { 
+      name: 'Game 15', 
+      description: 'Description for Game 15', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 29.99, 
+      rating: 4.8 
+    },
+    { 
+      name: 'Game 16', 
+      description: 'Description for Game 16', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 45.50, 
+      rating: 2.3 
+    },
+    { 
+      name: 'Game 17', 
+      description: 'Description for Game 17', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 0.99, 
+      rating: 3.8 
+    },
+    { 
+      name: 'Game 18', 
+      description: 'Description for Game 18', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 29.99, 
+      rating: 4.8 
+    },
+    { 
+      name: 'Game 19', 
+      description: 'Description for Game 19', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 45.50, 
+      rating: 2.3 
+    },
+    { 
+      name: 'Game 20', 
+      description: 'Description for Game 20', 
+      imageUrl: './assets/img/vgamestore_logo_blue.svg', 
+      price: 0.99, 
+      rating: 3.8 
+    }
   ];
 
   // Generating an array of numbers from 0 to the length of games array
