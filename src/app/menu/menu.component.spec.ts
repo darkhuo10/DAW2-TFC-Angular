81c0479c07abac4eda12e3f4d963d0c723711b25
Daniel Rodríguez Muñoz <daniel.ro.mu02@gmail.com>
2024-05-15 21:03:47 +0200

    menu lateral más feo que pegarle a un padre con una toalla sucia

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { MenuComponent } from './menu.component';
 
 describe('MenuComponent', () => {
@@ -19,5 +19,22 @@ describe('MenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle menu visibility', () => {
+    const menuToggle = fixture.debugElement.query(By.css('.menu-toggle'));
+    const dropdownMenu = fixture.debugElement.query(By.css('.dropdown-menu'));
+
+    expect(dropdownMenu.classes['open']).toBeFalsy();
+
+    menuToggle.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(dropdownMenu.classes['open']).toBeTruthy();
+
+    menuToggle.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(dropdownMenu.classes['open']).toBeFalsy();
   });
 });
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
