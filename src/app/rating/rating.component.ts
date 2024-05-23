2779b3f2fa276068070a4cfef52d7a1d51f8bb51
Marina Pintado <105634828+darkhuo10@users.noreply.github.com>
2024-05-23 09:39:12 +0000

    New rating component -> readonly + half stars when decimals

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -6,9 +6,22 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./rating.component.scss'] 
 })
 export class RatingComponent {
-  @Input() rating: number = 0; 
+  @Input() rating: number = 0;
+  stars: number[] = [1, 2, 3, 4, 5];
 
-  stars: number[] = Array.from({ length: 5 }, (_, i) => i + 1);
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  getStarType(index: number): string {
+    if (this.rating >= index + 1) {
+      return 'filled';
+    } else if (this.rating > index && this.rating < index + 1) {
+      return 'half';
+    } else {
+      return 'label';
+    }
+  }
 
   
 }
