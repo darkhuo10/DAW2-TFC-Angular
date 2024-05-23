815bfdfd2ea94590115f11647a47dce12eb1e979
Marina Pintado <105634828+darkhuo10@users.noreply.github.com>
2024-05-23 08:36:12 +0000

    not functional ratings, just to display the info

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -3,21 +3,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
-  styleUrl: './rating.component.scss'
+  styleUrls: ['./rating.component.scss'] 
 })
 export class RatingComponent {
   @Input() rating: number = 0; 
-  @Input() disabled: boolean = false;
 
   stars: number[] = Array.from({ length: 5 }, (_, i) => i + 1);
 
-  updateRating(star: number): void {
-    if (!this.disabled) {
-      if (this.rating === star) {
-        this.rating = 0;
-      } else {
-        this.rating = star;
-      }
-    }
-  }
+  
 }
