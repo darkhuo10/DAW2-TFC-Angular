d8b844211b6832543b4e1718f2c9853c315e2943
Marina Pintado <105634828+darkhuo10@users.noreply.github.com>
2024-05-24 10:08:45 +0000

    Reviews done

--- src/app/my-review/my-review.component.ts
+++ src/app/my-review/my-review.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-review',
   templateUrl: './my-review.component.html',
-  styleUrl: './my-review.component.scss'
+  styleUrls: ['./my-review.component.scss']
 })
 export class MyReviewComponent {
-
+  rating: string = "1.0"; // Initialize with string "1.0"
+  
+  onRatingChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const parsedValue = parseFloat(inputElement.value);
+    this.rating = parsedValue === Math.floor(parsedValue) ? parsedValue + ".0" : parsedValue.toString();
+  }
 }
