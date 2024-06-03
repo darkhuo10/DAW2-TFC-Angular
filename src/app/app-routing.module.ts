f120cbdd1466ea506535bd81a8ff9c71ffa53c9e
Marina Pintado <darkhuoyt@gmail.com>
2024-06-03 12:09:06 +0200

    components directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { LibraryComponent } from './library/library.component';
-import { WishlistComponent } from './wishlist/wishlist.component';
-import { MainComponent } from './main/main.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { GameDetailsComponent } from './game-details/game-details.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { LibraryComponent } from './components/library/library.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { MainComponent } from './components/main/main.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { GameDetailsComponent } from './components/game-details/game-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: 'home', component: MainComponent },
