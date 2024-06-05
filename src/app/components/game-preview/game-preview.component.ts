5c683499c4cb2be2b4aa9eac7da8c1b194c884df
Marina Pintado <darkhuoyt@gmail.com>
2024-06-05 13:50:52 +0200

    Card style fixed size, plus scroll for more info (need to check it) | Code commented due to error with the auth service

--- src/app/components/game-preview/game-preview.component.ts
+++ src/app/components/game-preview/game-preview.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Game } from '../../models/game.model';
 import { GameService } from '../../services/game.services';
-import { AuthService } from '../../services/auth.services';
+//import { AuthService } from '../../services/auth.services';
 
 @Component({
   selector: 'app-game-preview',
@@ -10,6 +10,7 @@ import { AuthService } from '../../services/auth.services';
   styleUrl: './game-preview.component.scss'
 })
 export class GamePreviewComponent {
+  
   loading = false;
   errorMessage = '';
   isAdminUser = false;
@@ -25,14 +26,31 @@ export class GamePreviewComponent {
 
   constructor(
     private router: Router,
-    private gameService: GameService,
-    private authService: AuthService
+    private gameService: GameService/*,
+    private authService: AuthService*/
   ){}
 
   ngOnInit(): void {
-    this.authService.isAdmin();
+    //this.authService.isAdmin();
   }
+  @ViewChild('card')
+  card!: ElementRef;
+  @ViewChild('scrollIndicator')
+  scrollIndicator!: ElementRef;
 
+  ngAfterViewInit() {
+    // Add a scroll event listener to the card
+    this.card.nativeElement.addEventListener('scroll', () => {
+      // Check if the card is scrolled
+      if (this.card.nativeElement.scrollTop > 0) {
+        // If scrolled, hide the scroll indicator
+        this.scrollIndicator.nativeElement.style.display = 'none';
+      } else {
+        // If not scrolled, show the scroll indicator
+        this.scrollIndicator.nativeElement.style.display = 'block';
+      }
+    });
+  }
   redirectToDetails(){
     this.router.navigate(['/game', this.game.id]);
   }
