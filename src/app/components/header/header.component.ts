ace0e23d576756bcf976e2fb23df3a141090064f
Daniel Rodríguez Muñoz <daniel.ro.mu02@gmail.com>
2024-06-16 14:24:24 +0200

    buscador terminado y barra lateral solo se muestra en ciertas pestañas

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { GameService } from '../../services/game.services';
+import { SharedService } from '../../services/shared.service';
+import { Game } from '../../models/game.model';
+import { Observable } from 'rxjs';
+import { LibraryService } from '../../services/library.service';
+import { WishlistService } from '../../services/wishlist.service';
+import { AuthService } from '../../services/auth.services';
 
 @Component({
   selector: 'app-header',
@@ -10,14 +17,45 @@ import { Router } from '@angular/router';
 // hooks order
 export class HeaderComponent {
   logo = "assets/img/vgamestore_logo_white.svg";
+  @ViewChild('filterText') filter!: ElementRef;
 
   constructor(
-    private router: Router
-  ) {}  
+    private router: Router,
+    private gameService: GameService,
+    private sharedService: SharedService,
+    private libraryService: LibraryService,
+    private wishlistService: WishlistService,
+    private authService: AuthService
+  ) {}
 
   checkUrl(): boolean {
     const url = this.router.url;
     let x = url == "/home" || url == "/wishlist" || url == "/library";
     return x;
+  }
+
+  filterGames(): void {
+    let call: Observable<any>
+    switch (this.router.url) {
+      case "/library": {
+        call = this.libraryService.getAllGames(this.authService.getCurrentUser());
+        break;
+      }
+      case "/wishlist": {
+        call = this.wishlistService.getAllGames(this.authService.getCurrentUser());
+        break;
+      }
+      default: {
+        call = this.gameService.getAllGames({"visible": true})
+      }
+    }
+    call.subscribe((games: Game[]) => {
+      console.log("filter: "+this.filter.nativeElement)
+      console.log(this.filter.nativeElement.value)
+      const filteredGames = games.filter(game => 
+        game.name.toLowerCase().includes((this.filter.nativeElement != null) ? this.filter.nativeElement.value.toLowerCase() : ''));
+      this.sharedService.setGames(filteredGames);
+      console.dir(this.sharedService.getGames());
+    });
   }
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -11,7 +12,8 @@ export class MenuComponent implements OnInit {
   isAdminUser = false;
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -20,6 +22,19 @@ export class MenuComponent implements OnInit {
 
   toggleMenu(state: boolean) {
     this.isMenuOpen = state;
+  }
+
+  cerrarSesion(): void {
+    this.toggleMenu(false);
+    localStorage.setItem('token', '');
+  }
+
+  shouldBeVisible(): boolean {
+    switch (this.router.url) {
+      case "/register": { return false; }
+      case "/login": { return false; }
+      default: { return true; }
+    }
   }
 
   isAdmin() {
