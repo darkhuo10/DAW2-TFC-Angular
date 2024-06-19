308b560290e7e4c858a0a466b1d26ae6889c1fec
Marina Pintado <darkhuoyt@gmail.com>
2024-06-19 17:11:43 +0200

    dark theme header & footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { ThemeService } from '../../services/theme.services';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
-  constructor (private themeService: ThemeService){}
-
-  dark: boolean = this.themeService.isDarkTheme();
+export class FooterComponent{
 }
